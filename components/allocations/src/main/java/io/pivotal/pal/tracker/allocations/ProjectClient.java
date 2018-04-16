273792a8449a76afd5ad2e987143d40fb6115296
pal4me <dfossgreen@relianceco.com>
2018-04-16 09:13:25 -0600

    Circuit Breaker Implementation

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,8 +1,18 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 public class ProjectClient {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final ConcurrentMap<Long, ProjectInfo> projectMap;
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
@@ -10,9 +20,18 @@ public class ProjectClient {
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
+        this.projectMap = new ConcurrentHashMap<Long, ProjectInfo>();
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo project = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        projectMap.put(projectId, project);
+        return project;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        logger.info("Getting project with id {} from cache", projectId);
+        return projectMap.get(projectId);
     }
 }
